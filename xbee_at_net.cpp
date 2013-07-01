fec36e8e5d55c6efa58396c96ee1300448d0523c
Austin <legotown@aol.com>
2013-07-01 14:14:17 -0700

    Implement serial number.

--- xbee_at_net.cpp
+++ xbee_at_net.cpp
@@ -1,6 +1,47 @@
 
 #include "xbee_at_cmd.h"
 
+class at_cmd_serial : public at_cmd_ro {
+   public:
+      at_cmd_serial() : at_cmd_ro("") {}
+
+   protected:
+      virtual int read(xbsh_state * state) {
+         state->send_AT("SH");
+         api_frame * high = state->read_AT();
+         if( !high ) {
+            printf("Didn't get a response\n");
+            return 1;
+         }
+         if( !high->ok() ) {
+            printf("Error: %s\n", high->get_error().c_str());
+            return 2;
+         }
+         state->send_AT("SL");
+         api_frame * low = state->read_AT();
+         if( !low ) {
+            printf("Didn't get a response\n");
+            return 1;
+         }
+         if( !low->ok() ) {
+            printf("Error: %s\n", low->get_error().c_str());
+            return 2;
+         }
+         std::vector<uint8_t> high_data = high->get_data();
+         std::vector<uint8_t> low_data  = low->get_data();
+         if( high_data.size() == 4 && low_data.size() == 4 ) {
+            printf("Serial: %02X%02X:%02X%02X:%02X%02X:%02X%02X\n",
+                  high_data[0], high_data[1], high_data[2], high_data[3],
+                  low_data[0],  low_data[1],  low_data[2],  low_data[3]);
+            return 0;
+         } else {
+            printf("Expected 8 and 8 bytes, got %zd and %zd\n", 
+                  high_data.size(), low_data.size());
+            return 3;
+         }
+      }
+};
+
 command ** net_short() {
    command ** result = new command*[3];
    command ** r = result;
@@ -69,7 +110,7 @@ command ** net() {
    *r++ = new command_child( "max-payload", fake_cmd );
    *r++ = new command_child( "destination", fake_cmd );
    *r++ = new command_child( "children",    fake_cmd );
-   *r++ = new command_child( "serial",      fake_cmd );
+   *r++ = new command_child( "serial", new at_cmd_serial() );
    *r++ = 0;
    return result;
 };
