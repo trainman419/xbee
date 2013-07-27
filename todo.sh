5d90828c6928c692ba4371d56d71fb7727d1087e
Austin <legotown@aol.com>
2013-07-27 01:01:41 -0700

    Fix todo script

--- todo.sh
+++ todo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TODO=`grep TODO *.h *.cpp *.c | wc -l`
-FAKE=`grep fake_cmd *.h *.cpp *.c | wc -l`
+TODO=`grep TODO *.h *.cpp | wc -l`
+FAKE=`grep fake_cmd *.h *.cpp | wc -l`
 echo "TODOs: $TODO"
 echo "Fake Commands: $FAKE"
