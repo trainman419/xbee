b52317dd630230d10d8d0e279b4eaba0e2e9c030
Austin <legotown@aol.com>
2013-07-29 00:18:05 -0700

    Update todo script.

--- todo.sh
+++ todo.sh
@@ -2,13 +2,4 @@
 
 TODO=`grep TODO *.h *.cpp | wc -l`
 TODO=$((TODO))
-FAKE=`grep fake_cmd *.h *.cpp | wc -l`
-CHILDREN=`grep 'new command_child(' *.h *.cpp | wc -l`
-CHILDREN=$((CHILDREN))
-FAKE=$(( FAKE - 2 ))
-
-PERCENT=$(( ( CHILDREN - FAKE ) * 100 ))
-PERCENT=$(( PERCENT / CHILDREN ))
-echo "TODOs:            $TODO"
-echo "Fake Commands:    $FAKE of $CHILDREN"
-echo "Percent complete: $PERCENT%"
+echo "TODOs: $TODO"
